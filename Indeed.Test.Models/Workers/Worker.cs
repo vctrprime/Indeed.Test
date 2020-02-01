0a755673eaba825bf14977636f45b9b0d0a570a7
vctrprime <vctr.prime@gmail.com>
2020-02-01 11:02:23 +0300

    first commit

---
+++ Indeed.Test.Models/Workers/Worker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indeed.Test.Models.Workers
+{
+    public class Worker : BaseEntity
+    {
+        public string Function { get; set; }
+
+        public Worker()
+        {
+            Function = this.GetType().FullName;
+        }
+    }
+}
